fe42df855196f365bf28a7b8dd0ac2c6f48eeb99
qwertyBBeers <gudgh1630@gmail.com>
2023-03-21 22:28:44 +0900

    Revert "save goal_pose"
    This reverts commit e418279333e8af7a24f94d673592118fc91fd919.

--- hair_D/omo_r1/omo_r1_bringup/src/hair_goal.py
+++ hair_D/omo_r1/omo_r1_bringup/src/hair_goal.py
@@ -19,57 +19,28 @@ def callback(msg):
     current_pose = msg
 
 def callbackRoom(data):
-    stage = data.data
+    stage = data
     #목적지 순서
     list_stage = []
-    print(stage)
-    print(type(stage))
-    if stage == 1:
-        hh_stage.c_x = -5.283970832824707
-        hh_stage.c_y = -1.8769316673278809
-        print("--------------------")
-    elif stage == 2:
-        hh_stage.c_x = 1
-        hh_stage.c_y = 1
-    elif stage == 3:
-        hh_stage.c_x = 1
-        hh_stage.c_y = 1
-    print(hh_stage.c_x)
-    for i in range(1, 9, 1):
+    
+    for i in 8:
         #목적지 순서당 가지는 Goal 값
         if i == 1:
-            hh_stage.c_x = hh_stage.c_x - 0.2
-            print(hh_stage.c_x)
-            hh_stage.c_y = hh_stage.c_y + 0
-            hh_stage.stage_goal = [hh_stage.c_x, hh_stage.c_y, -0.0017465238297493014, 0.9999984748260929]
+            hh_stage.stage_goal = [1, 2, 3, 4]
         elif i == 2:
-            hh_stage.c_x = hh_stage.c_x - 0.2
-            hh_stage.c_y = hh_stage.c_y + 0.5
-            hh_stage.stage_goal = [hh_stage.c_x, hh_stage.c_y, -0.0017465238297493014, 0.9999984748260929]
+            hh_stage.stage_goal = [1, 2, 3, 4]
         elif i == 3:
-            hh_stage.c_x = hh_stage.c_x - 0
-            hh_stage.c_y = hh_stage.c_y + 0.5
-            hh_stage.stage_goal = [hh_stage.c_x, hh_stage.c_y, -0.0017465238297493014, 0.9999984748260929]
+            hh_stage.stage_goal = [1, 2, 3, 4]
         elif i == 4:
-            hh_stage.c_x = hh_stage.c_x - 0
-            hh_stage.c_y = hh_stage.c_y + 0
-            hh_stage.stage_goal = [hh_stage.c_x, hh_stage.c_y, -0.0017465238297493014, 0.9999984748260929]
+            hh_stage.stage_goal = [1, 2, 3, 4]
         elif i == 5:
-            hh_stage.c_x = hh_stage.c_x - 3
-            hh_stage.c_y = hh_stage.c_y + 3
-            hh_stage.stage_goal = [hh_stage.c_x, hh_stage.c_y, 0.657537242689, 0.976968478788]
+            hh_stage.stage_goal = [1, 2, 3, 4]
         elif i == 6:
-            hh_stage.c_x = hh_stage.c_x - 3
-            hh_stage.c_y = hh_stage.c_y + 3
-            hh_stage.stage_goal = [hh_stage.c_x, hh_stage.c_y, 3, 4]
+            hh_stage.stage_goal = [1, 2, 3, 4]
         elif i == 7:
-            hh_stage.c_x = hh_stage.c_x - 3
-            hh_stage.c_y = hh_stage.c_y + 3
-            hh_stage.stage_goal = [hh_stage.c_x, hh_stage.c_y, 3, 4]
+            hh_stage.stage_goal = [1, 2, 3, 4]
         elif i == 8:
-            hh_stage.c_x = hh_stage.c_x - 3
-            hh_stage.c_y = hh_stage.c_y + 3
-            hh_stage.stage_goal = [hh_stage.c_x, hh_stage.c_y, 3, 4]
+            hh_stage.stage_goal = [1, 2, 3, 4]
         
         list_stage.append(hh_stage.stage_goal)
     
@@ -77,8 +48,6 @@ def callbackRoom(data):
 
 class hh_stage:
     stage_goal = []
-    c_x = 0
-    c_y = 0
 
 
 class GoalPose:
@@ -95,11 +64,11 @@ def move_to(stage_check):
 
     goal.target_pose.pose.orientation.x = 0
     goal.target_pose.pose.orientation.y = 0
-    goal.target_pose.pose.orientation.z = stage_check[2]
-    goal.target_pose.pose.orientation.w = stage_check[3]
+    goal.target_pose.pose.orientation.z = stage_check[0]
+    goal.target_pose.pose.orientation.w = stage_check[1]
 
-    goal.target_pose.pose.position.x = stage_check[0]
-    goal.target_pose.pose.position.y = stage_check[1]
+    goal.target_pose.pose.position.x = stage_check[2]
+    goal.target_pose.pose.position.y = stage_check[3]
     goal.target_pose.pose.position.z = 0
 
     # print(goal)
@@ -107,14 +76,10 @@ def move_to(stage_check):
 
 def goal_def(list_stage):
     goal_test = GoalPose()
-    error = 0.5
+    error = 0.1
 
-    for i in range(8):
+    for i in 8:
         stage_check = list_stage[i]
-        print("--------------------")
-        print(i)
-        goal_test.x = stage_check[0]
-        goal_test.y = stage_check[1]
 
     # for room in stage:
     #     if room == 101:
@@ -142,9 +107,7 @@ def goal_def(list_stage):
     #         goal_test.z = 0.657537242689
     #         goal_test.w = 0.753422042733
         move_to(stage_check)
-        # print("++++++++++++++++++++++++")
-        # print(goal_test.x)
-        # print("++++++++++++++++++++++++")
+
         while((abs(current_pose.pose.pose.position.x - goal_test.x) > error) or (abs(current_pose.pose.pose.position.y - goal_test.y) > error)):
             pass
 
