eb9c53160ad25bf82009442e1fb5987e7cec9426
qwertyBBeers <gudgh1630@gmail.com>
2023-04-06 13:39:55 +0900

    checking 4.6

--- hair_D/omo_r1/omo_r1_bringup/src/main_hair.cpp
+++ hair_D/omo_r1/omo_r1_bringup/src/main_hair.cpp
@@ -1,30 +1,30 @@
 #include <ros/ros.h>
 #include <std_msgs/String.h>
+#include <omo_r1_bringup/yolo_check.h>
+#include <omo_r1_bringup/navigation_check.h>
 
-std::string yolo_msg, bbangle_msg;
-void bbangle_callback(const std_msgs::String::ConstPtr& msg)
+//srv 관리 부
+bool yolo_srv(omo_r1_bringup::yolo_check::Request &req,
+              omo_r1_bringup::yolo_check::Response &res)
 {
-  bbangle_msg = msg->data;
+  res.yolo_checkpub = "stop";
+  req.yolo_checkpub = "start";
+
 }
 
-void yolo_topic(){
-    yolo_msg = "start";
-    pub.publish(yolo_msg);
+
+void callback(const std_msgs::String::ConstPtr& msg)
+{
+  ROS_INFO("I heard: [%s]", msg->data.c_str());
 }
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "main_haird");
+  ros::init(argc, argv, "my_subscriber");
   ros::NodeHandle nh;
 
-  ros::Subscriber sub = nh.subscribe("/bbangle_stop", 10, bbangle_callback);
-
-  ros::Rate rate(10); //Set the loop rate in 10Hz
-  while (ros::ok())
-  {
-    yolo_topic();
-    rate.sleep();
-  }
-  
+  ros::Subscriber sub = nh.subscribe("/my_topic", 10, callback);
+  ros::ServiceServer server = nh.advertiseService("yolo_check", yolo_srv);
+  ros::spin();
   return 0;
 }
